a6ecf4f2b1e5f4166142344a20c7ca89163dc953
Tom <344924008@qq.com>
2022-06-29 10:37:55 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,33 @@
-// swift-tools-version: 5.6
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "HBDNavigationBar",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HBDNavigationBar",
-            targets: ["HBDNavigationBar"]),
+            targets: ["HBDNavigationBar"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
+       
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HBDNavigationBar",
+            dependencies: [],
             path: ".",
-            sources : ["HBDNavigationBar"],
-            dependencies: [
-            ]),
+            sources: ["HBDNavigationBar"],
+            linkerSettings: [
+                .linkedFramework("UIKit")
+            ]
+        ),
        
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
